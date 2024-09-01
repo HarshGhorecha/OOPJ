79693abff4c70dddc243bb5a724c5a46cf038b0a
HarshGhorecha <harshgp5123@gmail.com>
2024-09-01 10:49:02 +0530

    Find max number of 3 values

---
+++ Find max number.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		
+		if (a>b && a>c){
+		    System.out.println("a is maxi");
+		    }else if(b>a && b>c){
+		        System.out.println("b is maxi");
+		        }else{
+		            System.out.println("c is maxi");		
+	}
+}
+}
