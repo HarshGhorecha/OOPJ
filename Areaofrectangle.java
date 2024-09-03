fe548488beffbbe869fb2db9539e57f4e6aa2089
HarshGhorecha <harshgp5123@gmail.com>
2024-09-03 19:23:11 +0530

    Lab 2 (prg 3) area of rectangle

---
+++ Areaofrectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Areaofra
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+
+		int hi=sc.nextInt();
+		int wi=sc.nextInt();
+
+		System.out.println("Area of ra:"+(hi+wi));
+	}
+}
