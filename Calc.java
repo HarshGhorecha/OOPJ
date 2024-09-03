b19e8151c030dbf0df7da3072cfe70e197b19eca
HarshGhorecha <harshgp5123@gmail.com>
2024-09-03 19:19:27 +0530

    Lab 2 prg 1

---
+++ Calc.java
@@ -0,0 +1,14 @@
+class Calc
+{
+	public static void main(String args[]){
+		int a=10+20*30;
+		System.out.println("cheak:"+a);
+		int b=100/10*100;
+		System.out.println("cheak:"+b);
+		int c=5*4/4%3;
+		System.out.println("cheak:"+c);
+		int d=100+200/10-3*20;
+		System.out.println("cheak:"+d);
+
+	}
+}
