42b12fe7beb6f09971d55eb7644df10d01694770
HarshGhorecha <harshgp5123@gmail.com>
2024-12-03 23:58:17 +0530

    Create All_Loops_Demo.java - [4 Marks]

---
+++ All_Loops_Demo.java
@@ -0,0 +1,41 @@
+public class All_Loops_Demo {
+    public static void main(String args[])
+    {
+        System.out.println(" 4 Marks ");
+        /* WHILE LOOP DEMO 
+            => WHEN NUMBER OF INTERATION IS NOT FIXED IT IS RECOMANDEDE TO USE WHILE LOOP
+            => WHILE LOOP IS "ENTRY CONTROLLED LOOP"
+            int num = 1;
+
+        while(num < 10)
+        {
+            System.out.println(num);
+            num++;
+        }
+        */
+
+        /*DO-WHILE:
+         => DO WHILE LOOP EXECUTED AT LEAST ONCE BEACAUSE COMDITION CHECK AFTER LOOP BODY
+         => DO WHILE IS "EXIT CONTROLLED LOOP" 
+        
+         int num = 1;
+        do
+        {
+            System.out.println(num);
+            num++;
+        }while(num < 10);
+        */
+        
+        /*
+        FOR LOOP:
+        => FOR LOOP IS USED TO ITREATE PART OF PROGRAMM SERVAL TIME
+        => IF THE NUMBER OF ITERATION IS FIXED IT IS RECOMMANDEE TO USE FOR LOOP
+        =>FOR(INTIALIZTION; ITREATION; INCREAMENT/ DECREAMENT){}
+
+        for(int i=0; i<10; i++)
+        {
+            System.out.println(i);
+        }
+        */
+    }
+}
